90ec22d2ce65557bb8c90d75019ca71fbef52b0f
권현재 <kk_jae@naver.com>
2023-04-12 20:15:54 +0900

    feat:불필요한 코드 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import Home from "./Market/home";
 
 export default function Main() {
-  const router = useRouter();
-
   return <Home />;
 }
