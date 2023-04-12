96c0aec0d21872025f83fcd41051bb62d18be3de
권현재 <kk_jae@naver.com>
2023-04-12 16:22:30 +0900

    fix:next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "project-kk-jae",
+
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/Market": { page: "/Market" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
